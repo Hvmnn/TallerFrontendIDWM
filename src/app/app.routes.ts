2185279ccac58ffc3a0112834f3dc73b3740cde3
Thetrolxs <moralesharnisch@gmail.com>
2025-01-02 07:39:27 -0300

    feat: add rounting and conf login in app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { ProductListComponent } from './products/components/product-list/product-list.component';
 import { ProductFormComponent } from './products/components/product-form/product-form.component';
 import { ProductDetailComponent } from './products/components/product-detail/product-detail.component';
+import { LoginComponent } from './auth/page/login/login.component';
 
 export const routes: Routes = [
+  { path: 'login', component: LoginComponent},
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductListComponent },
   { path: 'products/new', component: ProductFormComponent },
