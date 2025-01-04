0fb98cceb4bd899dbb318cf517c0f1577e9b4d5f
Thetrolxs <moralesharnisch@gmail.com>
2025-01-04 18:50:44 -0300

    fix: register interface

--- src/app/auth/interface/register.ts
+++ src/app/auth/interface/register.ts
@@ -1,15 +1,20 @@
 export interface Register {
-    message: string;
+    user:  User;
     token: string;
-    registerUser: RegisterUser;
 }
 
-export interface RegisterUser{
-    rut: string;
-    name: string;
+export interface User {
+    id:       number;
+    rut:      string;
+    name:     string;
     birthday: Date;
-    Email: string;
-    GenderId: number;
-    password: string;
-    confirmPassword: string;
+    email:    string;
+    isActive: boolean;
+    gender:   Gender;
+    role:     Gender;
+}
+
+export interface Gender {
+    id:   number;
+    type: string;
 }
